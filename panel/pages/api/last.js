c3f8fe05a8c45d013997a6dd6e5917f438108f24
Official Bitcoin do Paraguai <jhonmaxell48@gmail.com>
2025-09-04 12:44:00 -0300

    Update last.js

--- panel/pages/api/last.js
+++ panel/pages/api/last.js
@@ -1,16 +1,5 @@
-let lastData = {};
-
 export default function handler(req, res) {
-  if (req.method === 'POST') {
-    const { id, type, ...data } = req.body;
-    lastData[id] = { id, type, ...data, timestamp: Date.now() };
-    return res.status(200).end();
-  }
-
-  if (req.method === 'GET') {
-    const { id } = req.query;
-    return res.json(lastData[id] || {});
-  }
-
-  res.status(405).end();
+  const { id } = req.query;
+  const data = global.trackerData?.[id] || {};
+  res.status(200).json(data);
 }
